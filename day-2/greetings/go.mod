2d66874ebfbec27402dee6c72925ade167baeed6
BruceGoodGuy <khoauth@gmail.com>
2024-08-06 22:25:04 +0700

    100 days go #1

---
+++ day-2/greetings/go.mod
@@ -0,0 +1,3 @@
+module day-2/greetings
+
+go 1.22.3
---
+++ day-2/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"day-2/greetings"
+
+	"fmt"
+)
+
+func main() {
+	message := greetings.Hello("Con vit")
+
+	fmt.Println(message)
+}
